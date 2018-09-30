69b992e1a17f0e002568d07f8c9020628132072c
Laurent Hénault-Brunet <laurent.henault.brunet@gmail.com>
2018-09-30 15:06:04 -0400

    Cleanup charge/multiplejump/walljump look ok

--- Cadriciel_2018_v5/Assets/Sample Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Cadriciel_2018_v5/Assets/Sample Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -7,56 +7,48 @@ public class PlatformerCharacter2D : MonoBehaviour
 	bool facingRight = true;							// For determining which way the player is currently facing.
 
 	[SerializeField] float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
-	[SerializeField] float jumpForce = 400f;			// Amount of force added when the player jumps.	
 
-	public float jumpSpeed = 15f;
+	[SerializeField] float jumpSpeed = 15f;
 
-	float maxCrouchJumpForce = 400f;
-
-	public float maxCrouchJumpSpeed = 15f;
+	[SerializeField] float chargedJumpSpeed = 10f;
 
 	[Range(0, 1)]
 	[SerializeField] float crouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 	
 	[SerializeField] bool airControl = false;			// Whether or not a player can steer while jumping;
 	[SerializeField] LayerMask whatIsGround;			// A mask determining what is ground to the character
-
-	[SerializeField] LayerMask whatIsWall;
-	
 	Transform groundCheck;								// A position marking where to check if the player is grounded.
 	float groundedRadius = .2f;							// Radius of the overlap circle to determine if grounded
-
-	public float wallRadius = .2f;
 	bool grounded = false;								// Whether or not the player is grounded.
+	
+	Transform wallCheck;
+	[SerializeField] LayerMask whatIsWall;
+	float wallRadius = .2f;
+	bool onWall = false;
+
 	Transform ceilingCheck;								// A position marking where to check for ceilings
 	float ceilingRadius = .01f;							// Radius of the overlap circle to determine if the player can stand up
 	Animator anim;										// Reference to the player's animator component.
 
-	Transform wallCheck;
-
-	bool onWall = false;
-
 	long initialCrouchTs;
 
-	public float maxChargeJump = 10000f * 2000f;
+	[SerializeField] float maxChargeJumpTimeMs = 1000f;
+	[SerializeField] float wallJumpDebounceMs = 200f;
 
-	public float wallJumpDebounceTime = 10000f * 200f;
-	
 	long initialWallJumpTs = 0;
+	float jumpCharge = 0f;
 
-	public float jumpCharge = 0f;
+	[SerializeField] Image jumpChargeBar;
 
-	public Image jumpChargeBar;
+	[SerializeField] GameObject jumpBarCanvas;
 
-	public GameObject jumpBarCanvas;
-
-	public int maxJumps = 3;
+	[SerializeField] int maxJumps = 3;
 
 	int currentJumps = 0;
 
-	public Vector2 wallPushSpeedRight = new Vector2( 10f, 15f);
+	[SerializeField] Vector2 wallPushSpeedRight = new Vector2( 10f, 15f);
 
-	public Vector2 wallPushSpeedLeft = new Vector2( -10f, 15f);
+	[SerializeField] Vector2 wallPushSpeedLeft = new Vector2( -10f, 15f);
 
     void Awake()
 	{
@@ -64,18 +56,15 @@ public class PlatformerCharacter2D : MonoBehaviour
 		groundCheck = transform.Find("GroundCheck");
 		ceilingCheck = transform.Find("CeilingCheck");
 		wallCheck = transform.Find("WallCheck");
+
 		anim = GetComponent<Animator>();
 	}
 
 
 	void FixedUpdate()
 	{
+		//Checks if the player is near and facing a wall
 		onWall = Physics2D.OverlapCircle(wallCheck.position, wallRadius, whatIsWall);
-
-		if (onWall)
-		{
-			Debug.Log("OnWALL TRUE");
-		}
 
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
@@ -88,14 +77,12 @@ public class PlatformerCharacter2D : MonoBehaviour
 
 	public void Move(float move, bool crouch, bool jump)
 	{
-		bool wallDebounce = true;
+		bool wallJumpDebounce = true;
 
-		if ((System.DateTime.Now.Ticks - initialWallJumpTs) > wallJumpDebounceTime)
+		if ((TicksToMs(System.DateTime.Now.Ticks) - initialWallJumpTs) > wallJumpDebounceMs)
 		{
-			wallDebounce = false;
+			wallJumpDebounce = false;
 		}
-
-		
 
 		// If crouching, check to see if the character can stand up
 		if(!crouch && anim.GetBool("Crouch"))
@@ -108,14 +95,13 @@ public class PlatformerCharacter2D : MonoBehaviour
 		//Character just started crouching
 		if (!anim.GetBool("Crouch") && crouch && !jump && grounded)
 		{
-			initialCrouchTs = System.DateTime.Now.Ticks;	
+			initialCrouchTs = TicksToMs(System.DateTime.Now.Ticks);	
 			jumpCharge = 0;
 			jumpBarCanvas.SetActive(true);
 		}
 		else if (anim.GetBool("Crouch") && crouch)
 		{
-			jumpCharge = Math.Min((System.DateTime.Now.Ticks - initialCrouchTs) / maxChargeJump, 1);
-
+			jumpCharge = Math.Min((TicksToMs(System.DateTime.Now.Ticks) - initialCrouchTs) / maxChargeJumpTimeMs, 1);
 			jumpChargeBar.fillAmount = jumpCharge;
 		}
 		else if (!crouch)
@@ -125,16 +111,12 @@ public class PlatformerCharacter2D : MonoBehaviour
 		}
 		// Set whether or not the character is crouching in the animator
 		anim.SetBool("Crouch", crouch && grounded);
-	
 
-	
-
-		if (!wallDebounce)
+		if (!wallJumpDebounce)
 		{
 			//only control the player if grounded or airControl is turned on
 			if(grounded || airControl)
 			{
-				Debug.Log("Move : " + move);
 				// Reduce the speed if crouching by the crouchSpeed multiplier
 				move = (crouch ? move * crouchSpeed : move);
 
@@ -156,19 +138,16 @@ public class PlatformerCharacter2D : MonoBehaviour
 
 			// If the player should jump...
 			if (grounded && jump) {
-				// Debug.Log("defaultJump");
 				// Add a vertical force to the player.
 				anim.SetBool("Ground", false);
 				jumpBarCanvas.SetActive(false);
 				currentJumps = 1;
 
-				GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpSpeed + jumpCharge * maxCrouchJumpSpeed);
-	//             GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce + jumpCharge * maxCrouchJumpForce));
+				GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpSpeed + jumpCharge * chargedJumpSpeed);
 			}
 			else if (!grounded && onWall && jump)
 			{
-				// Debug.Log("wallJump");
-				initialWallJumpTs = System.DateTime.Now.Ticks;
+				initialWallJumpTs = TicksToMs(System.DateTime.Now.Ticks);
 
 				if (facingRight)
 				{
@@ -179,14 +158,11 @@ public class PlatformerCharacter2D : MonoBehaviour
 					GetComponent<Rigidbody2D>().velocity = wallPushSpeedRight;
 				}
 				Flip();
-				
 			}
 			else if (!grounded && jump && currentJumps < maxJumps)
 			{
-				// Debug.Log("extra jump");
 				currentJumps++;
 				GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpSpeed);
-	//         GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
 			}
 		}
 	}
@@ -201,5 +177,10 @@ public class PlatformerCharacter2D : MonoBehaviour
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+	}
+
+	private long TicksToMs(long ticks)
+	{
+		return ticks / TimeSpan.TicksPerMillisecond;
 	}
 }
